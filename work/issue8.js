8870df5ad146e5b983cbfb04ccee2095ece66c1c
songhyeongju <ardieyes@gmail.com>
2022-10-05 18:30:08 +0900

    문제풀이

--- work/issue8.js
+++ work/issue8.js
@@ -1,7 +1,16 @@
+const fs = require('fs');
 
-    const issue8 = "work";
+function issue(number) {
+  const pokemon = JSON.parse(
+    fs.readFileSync('../fromDB/pokemon.json', 'utf-8')
+  );
+  const pokemons = pokemon.find((data) => {
+    return data.id === number;
+  });
+  console.log(pokemons);
+}
 
-    module.exports = {
-      issue8
-    };
-  
+const issue8 = issue(25);
+module.exports = {
+  issue8,
+};
