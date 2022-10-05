73ec2a94a4ff500742c0158573a93b4b0a970e2b
LeeSangHo <4leaf.lsh@gmail.com>
2022-10-05 19:36:42 +0900

    add code issue

--- work/issue13.js
+++ work/issue13.js
@@ -1,7 +1,28 @@
+const fs = require("fs");
+const path = require("path");
 
-    const issue13 = "work";
+let issue13 = null;
 
-    module.exports = {
-      issue13
-    };
-  
+function Issue13({ id, name, koreaName }) {
+  this.id = id;
+  this.name = name;
+  this.koreaName = koreaName;
+}
+
+const JsonFile = fs.readFileSync(
+  path.join(__dirname, "..", "/fromDB", "pokemon.json"),
+  "utf-8"
+);
+const JsonData = JSON.parse(JsonFile);
+
+JsonData.forEach((data) => {
+  if (data.id === 13 + 17) {
+    issue13 = new Issue13(data);
+  }
+});
+
+module.exports = {
+  issue13,
+};
+
+//
