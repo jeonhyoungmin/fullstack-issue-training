e91f8744f3e4e0b7aafe5084ab312f5d8a6ffa4b
kongukjae <kongukjae@gmail.com>
2022-10-05 14:55:57 +0900

    webpack config 파일에 resolve, module 셋팅 긁어옴 -> 정리 필요함 #issue2 걸어두기

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,33 @@ module.exports = (_env, argv) => {
     entry: {
       main: './src/index.js',
     },
+    resolve: {
+      extensions: ['js', 'jsx', 'json'],
+      alias: {
+        '@components': getAbsolutePath('src/components/'),
+        '@contexts': getAbsolutePath('src/contexts/'),
+        '@hooks': getAbsolutePath('src/hooks/'),
+        '@pages': getAbsolutePath('src/pages/'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+                cacheCompression: false,
+                envName: isProd ? 'production' : 'development'
+              }
+            }
+          ]
+        }
+      ]
+    },
     output: {
       path: getAbsolutePath('dist'),
       filename: 'assets/js/[name].[contenthash:8].js',
