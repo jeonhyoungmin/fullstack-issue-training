f83741db3174f28630151b4c35803f40469e8bf2
MinhoYoo <minhoyoo1899@gmail.com>
2022-10-05 18:06:42 +0900

    test

--- work/issue12.js
+++ work/issue12.js
@@ -1,7 +1,38 @@
+//console.log('a');
 
-    const issue12 = "work";
+const jsonData = require("../fromDB/pokemon.json");
+//console.log(jsonData);
+//console.log(jsonData[0]);
 
-    module.exports = {
-      issue12
-    };
-  
+const issueNum = 12;
+const numDat = 17;
+
+
+const issue12 = (id, name, koreaName) => {
+  this.id = id;
+  this.name = name;
+  this.korea = koreaName;
+  const returnValue = `id = ${id} name = ${name} koreaName = ${koreaName}`;   
+  return returnValue;
+}
+
+console.log(issue12(12, "MinhoYoo", "유민호"));
+console.log(jsonData[issueNum + numDat - 1]);
+
+
+
+// class issue12 {
+//   constructor(id, name, koreaname) {
+//     this.id = id;
+//     this.name = name;
+//     this.koreaname = koreaname;
+//   }
+// }
+
+//const issueA = new issue12();
+
+
+
+module.exports = {
+  issue12,
+};
