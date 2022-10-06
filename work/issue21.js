a05d48530d1aa05409070843892d2e3432e77256
Hwayeon Choi <hwayeon.choi11@gmail.com>
2022-10-06 11:57:56 +0900

    issue21 solution

--- work/issue21.js
+++ work/issue21.js
@@ -1,7 +1,18 @@
+const pokemonData = require("../fromDB/pokemon.json");
 
-    const issue21 = "work";
+class PokemonDataClass {
+  constructor(id, name, koreaName) {
+    this.id = id;
+    this.name = name;
+    this.koreaName = koreaName;
+  }
+}
 
-    module.exports = {
-      issue21
-    };
-  
+const issue21 = new PokemonDataClass(pokemonData[21+16].id, pokemonData[21+16].name, pokemonData[21+16].koreaName);
+
+
+module.exports = {
+  issue21
+};
+
+console.log(issue21);
