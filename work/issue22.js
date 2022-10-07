202bb2bf806ff68963039c4221c43534e30b8583
YongJun Han <yongjun@YongJuns-MacBook-Pro.local>
2022-10-07 11:06:18 +0900

    issue22 complete

--- work/issue22.js
+++ work/issue22.js
@@ -1,7 +1,20 @@
+const fs = require('fs');
+const pokemonData = require("../fromDB/pokemon.json")
 
-    const issue22 = "work";
+class pokemon39 {
+  constructor(id,name,koreaName) {
+    this.id = id;
+    this.name = name;
+    this.koreaName = koreaName;
+  }
+}
 
-    module.exports = {
-      issue22
-    };
-  
+const issue22 =new pokemon39(pokemonData[38].id,pokemonData[38].name,pokemonData[38].koreaName)
+console.log(issue22)
+
+fs.writeFile('./issue22.json', JSON.stringify((issue22),null,2) , err => {if(err) throw err})
+
+
+module.exports = {
+  issue22
+};
