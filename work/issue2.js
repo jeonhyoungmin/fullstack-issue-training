b686bb9d0959c13529fc3cb07404e715af536a2b
geunsoo <geunsoo1106@gmail.com>
2022-10-12 19:09:23 +0900

    김근수 과제 완료

--- work/issue2.js
+++ work/issue2.js
@@ -1,7 +1,14 @@
-
-    const issue2 = "work";
-
-    module.exports = {
-      issue2
-    };
-  
+const Pokemon = require("../fromDB/pokemon.json");
+class PokemonData {
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.koreaName = data.koreaName;
+  }
+}
+const issue2 = new PokemonData(Pokemon[20]);
+// const issue2 = Pokemon[20];
+console.log(issue2);
+module.exports = {
+  issue2,
+};
